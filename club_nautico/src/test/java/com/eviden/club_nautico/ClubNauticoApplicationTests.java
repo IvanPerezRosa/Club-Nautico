33b6773a8dc5a313cb751ab22a9b80b812634833
Iván Pérez Rosa <145558979+IvanPerezRosa@users.noreply.github.com>
2024-06-05 09:35:09 +0200

    Upload Proyecto Creado

---
+++ club_nautico/src/test/java/com/eviden/club_nautico/ClubNauticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviden.club_nautico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
