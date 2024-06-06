31064eaacbc6141fea4fc04af99259affd139086
Ivan Perez Rosa <ivan.perez@eviden.com>
2024-06-06 12:57:42 +0200

    Actualizado CRUD de entidades Socio y Barco

--- club_nautico/src/main/java/com/eviden/club_nautico/controller/BarcoController.java
+++ club_nautico/src/main/java/com/eviden/club_nautico/controller/BarcoController.java
@@ -3,6 +3,7 @@ package com.eviden.club_nautico.controller;
 import com.eviden.club_nautico.DTOs.BarcoDTO;
 import com.eviden.club_nautico.entity.Barco;
 import com.eviden.club_nautico.services.BarcoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,31 @@ public class BarcoController {
     public ResponseEntity<List<BarcoDTO>> getAllBarcos() {
         List<BarcoDTO> barcos = barcoService.getAllBarcos();
         return new ResponseEntity<>(barcos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BarcoDTO> getBarcoById(@PathVariable long id) {
+        BarcoDTO barcoDTO = barcoService.getBarcoById(id);
+        return new ResponseEntity<>(barcoDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllBarcos() {
+         barcoService.deleteAllBarcos();
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBarcoById(@PathVariable long id){
+        barcoService.deleteBarcoById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BarcoDTO> updateBarco(@PathVariable Long id, @Valid @RequestBody BarcoDTO barcoDTO) {
+        BarcoDTO updatedBarcoDTO = barcoService.updateBarco(id, barcoDTO);
+        return new ResponseEntity<>(updatedBarcoDTO, HttpStatus.OK);
     }
 
 }
